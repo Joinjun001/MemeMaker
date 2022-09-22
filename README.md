c5c6974eac0ccaf12f7fc9734ce7999f9dd11a96
Joinjun001 <109087027+Joinjun001@users.noreply.github.com>
2022-09-22 22:51:00 +0900

    0.11

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 0.1 NomadCoder에서 만든 초기 MemeMaker. 앞으로 업데이트 해야 될거
 
 폰트크기, 종류, 색깔 변경할수 있게 하기
+
+0.11 폰트크기, fill stroke, bold normal 변경할수 있게 만듬
+
+> 폰트 색깔, 종류 변경가능하게 만들고 css 수정하기.
--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const fileInput = document.getElementById("file");
 const textInput = document.getElementById("text");
 
+const fontSize = document.getElementById("font-size");
+const fontWeight = document.getElementById("font-weight");
+const fontFill = document.getElementById("font-fill");
+
 const saveBtn = document.getElementById("save");
 const destroyBtn = document.getElementById("destroy-btn");
 const modeBtn = document.getElementById("mode-btn");
@@ -20,10 +24,10 @@ canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 ctx.lineCap = "round";
-
-
 let isPainting = false;
 let isFilling = false;
+let isFontFilling = true;
+
 
 
 
@@ -112,14 +116,27 @@ function onFileChange(event) {
 function onDoubleClick(event) {
     if (text !== ""){
         ctx.save();
-    const text = textInput.value;
-    ctx.lineWidth = 1;
-    ctx.font = "68px serif";
-    ctx.strokeText(text, event.offsetX, event.offsetY);
+    if (isFontFilling) {
+        
+        const text = textInput.value;
+        ctx.lineWidth = 1;
+        ctx.font = `${fontWeight.value} ${fontSize.value}px serif`;
+        ctx.fillText(text, event.offsetX, event.offsetY);
+        
+    }
+    else {
+        
+        const text = textInput.value;
+        ctx.lineWidth = 1;
+        ctx.font = `${fontWeight.value} ${fontSize.value}px serif`;
+        ctx.strokeText(text, event.offsetX, event.offsetY);
+        
+    }
     ctx.restore();
     }
     
 }
+    
 
 function onSaveClick() {
     const url = canvas.toDataURL();
@@ -129,13 +146,33 @@ function onSaveClick() {
     a.click();
 }
 
+function onFontSizeChange(event){
+    ctx.fontSize = event.target.value;
+}
+function onFontWeightChange(event) {
+    ctx.fontWeight = event.target.value;
+}
+
+function onFontFillChange(event) {
+    if (fontFill.value === "Fill") {
+        isFontFilling = true;
+    }
+    else {
+        isFontFilling = false;
+    }
+}
+
 canvas.addEventListener("dblclick", onDoubleClick); 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 canvas.addEventListener("click", onCanvasClick);
+
 lineWidth.addEventListener("change", onLineWidthChange);
+fontSize.addEventListener("change", onFontSizeChange);
+fontWeight.addEventListener("change", onFontWeightChange);
+fontFill.addEventListener("change", onFontFillChange);
 color.addEventListener("change", onColorChange);
 
 colorOptions.forEach(color => color.addEventListener("click", OnColorClick));
