74a9b1867cbb3209af7640222e22a503230f54b7
Joinjun001 <109087027+Joinjun001@users.noreply.github.com>
2022-09-24 11:54:13 +0900

    0.13

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ function onFileChange(event) {
     const image = new Image();
     image.src = url;
     image.onload = function(){
-        ctx.drawImage(image, 0, 0, 800, 800);
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     }
     fileInput.value = null;
 }
